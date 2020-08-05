34bffbd30bba7c58d221b824409411706a7860ff
shehani <janadari1995@gmail.com>
2020-08-05 22:42:33 +0530

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
-  constructor() {}
+  htmlStr: string;
+name: any;
+namee:any;
+nameee:any;
+  constructor(public httpClient: HttpClient,private router: Router) {}
+  ngOnInit(){
+  }
 
-}
+  sendPostRequest() {
+    console.log("name",this.name)
+    // const req = {
+    //   experience : '4',
+    //   test_score : '4',
+    //   interview_score:'5'
+    
+    // }
+  
+    // this.httpClient.post('http://127.0.0.1:5000/predict',req).subscribe(data => {
+   
+    //   console.log(data);
+    // })
+  
+  const formData = new FormData();
+  formData.append("experience", this.name);
+  formData.append("test_score", this.namee);
+  formData.append("interview_score", this.nameee);
+  
+    this.httpClient.post('http://127.0.0.1:5000/predict',formData,{responseType: 'text'}).subscribe(data => {
+      console.log("jjj",data);
+      this.htmlStr = data;
+      console.log("thisss",this.htmlStr)
+    }) ,
+     
+      error => console.log('Error from backend API', +error);
+  
+  }
+  }
+  
